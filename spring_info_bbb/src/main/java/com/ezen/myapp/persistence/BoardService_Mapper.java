0ffc84ef87e2dafe9e1f97dcd99733381fbc35d9
Kimsm1 <ksm2683@gmail.com>
2021-11-17 14:31:58 +0900

    create : project

---
+++ spring_info_bbb/src/main/java/com/ezen/myapp/persistence/BoardService_Mapper.java
@@ -0,0 +1,53 @@
+package com.ezen.myapp.persistence;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import com.ezen.myapp.domain.BoardVo;
+import com.ezen.myapp.domain.CommentVo;
+import com.ezen.myapp.domain.SearchCriteria;
+
+public interface BoardService_Mapper {
+	
+	//���̹�Ƽ���� ����� �޼ҵ带 �����Ѵ�
+	
+	public int boardInsert(HashMap<String,Object> hm);
+
+	public int boardTotalCount(SearchCriteria scri);
+	
+	public int boardTotalCount2(HashMap<String,Object> hm);
+	
+	public ArrayList<BoardVo> boardSelectAll(SearchCriteria scri);
+	
+	public ArrayList<BoardVo> boardSelectAll2(HashMap<String,Object> hm);
+
+	public BoardVo boardSelectOne(int bidx);
+
+	public ArrayList<CommentVo> commentSelectAll(int bidx);
+
+	public int commentInsert(CommentVo cv);
+	
+	public int commentDel(int cidx);
+	
+	public ArrayList<CommentVo> commentMo(HashMap<String,Integer> hm);
+
+	public int commentTotalPage(int bidx);
+	
+	public int boardModify(HashMap<String,Object> hm);
+	
+	public int boardDelete(HashMap<String,Object> hm);
+
+	public boolean plusCnt(int bidx);
+
+	public int updateReplyCnt(int bidx);
+
+	public int updatehitCnt(int bidx);
+
+
+
+
+	
+
+
+
+}
